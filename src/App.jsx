8f667efd463f695e3af9e8083eada5652f7917e3
Satyajit Samal <samalsatyajit668@gmail.com>
2024-08-29 07:56:15 +0530

    added button to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Hey, this is Satyajit Samal
+      <Button>Login</Button>
     </>
   )
 }
