b0c0a9d63a91a9cb4ec6e798dd7bb270cbd0944e
Satyajit Samal <samalsatyajit668@gmail.com>
2024-08-29 08:50:59 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layouts/app-layout'
+import LandingPage from './pages/landing'
+import Onboarding from './pages/onboarding'
+import JobPage from './pages/job'
+import PostJob from './pages/post-job'
+import SavedJob from './pages/saved-job'
+import MyJobs from './pages/my-jobs'
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding />
+      },
+      {
+        path: '/job',
+        element: <JobPage />
+      },
+      {
+        path: '/job/:id',
+        element: <JobPage />
+      },
+      {
+        path: '/post-job',
+        element: <PostJob />
+      },
+      {
+        path: '/saved-job',
+        element: <SavedJob />
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />
+      }
+    ]
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Button>Login</Button>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+import { Outlet } from 'react-router-dom';
+
+const AppLayout = () => {
+    return (
+        <>
+        AppLayout
+        <Outlet />
+        </>
+    );
+}
+
+export default AppLayout;
