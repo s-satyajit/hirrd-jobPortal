7919d4930d89125eee29e0918703b8312285a8b9
Satyajit Samal <samalsatyajit668@gmail.com>
2024-11-29 23:03:47 +0530

    wip

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 
 const LandingPage = () => {
-    return (
-        <>
-        Landing Page
-        </>
-    )
-}
+  return (
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20" >
+      <section className="text-center" >
+        <h1 className="flex flex-col items-center justify-center" >
+          Find Your Dream Job{" "}
+          <span>
+            and get{" "}
+            <img
+              src="/logo.png"
+              alt="Hirrd logo"
+              className="h-14 sm:h-24 lg:h-32"
+            />
+          </span>
+        </h1>
+      </section>
+      <div>
+        {/* buttons */}
+        {/* carousel */}
+      </div>
+
+      {/* banner */}
+
+      <section>{/* cards */}</section>
+
+      {/* Accordion */}
+    </main>
+  );
+};
 
 export default LandingPage;
