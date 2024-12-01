11c23dbf8cd2c896a972635308eb5b4484980315
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-01 21:02:23 +0530

    updated apiJobs page adding logic

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,9 +1,15 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
-export async function getJobs(token, {location, company_id, searchQuery}) {
+export async function getJobs(token, { location, company_id, searchQuery }) {
   const supabase = await SupabaseClient(token);
 
   let query = supabase.from("jobs").select("*");
+
+  if (location) query = query.eq("location", location);
+
+  if (company_id) query = query.eq("company_id", company_id);
+
+  if (searchQuery) query = query.ilike("title", `%${searchQuery}%`);
 
   const { data, error } = await query;
 
