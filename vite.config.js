500ebe64f801e3ebfca69dfb720d736739e2994b
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-02 16:38:41 +0530

    chore: update .gitignore and fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { dotenv } from "dotenv"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
